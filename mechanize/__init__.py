8a974c5b27727ea6cd9b04d7631326f46541ba6c
sfall <sfall@my.tnstate.edu>
2014-02-26 23:20:26 -0600

    Cleared up PEP 8 spacing issues

--- mechanize/__init__.py
+++ mechanize/__init__.py
@@ -111,7 +111,7 @@ __all__ = [
     'SubmitControl',
     'TextControl',
     'TextareaControl',
-    ]
+]
 
 import logging
 import sys
@@ -120,16 +120,16 @@ from ._version import __version__
 
 # high-level stateful browser-style interface
 from ._mechanize import \
-     Browser, History, \
-     BrowserStateError, LinkNotFoundError, FormNotFoundError
+    Browser, History, \
+    BrowserStateError, LinkNotFoundError, FormNotFoundError
 
 # configurable URL-opener interface
 from ._useragent import UserAgentBase, UserAgent
 from ._html import \
-     Link, \
-     Factory, DefaultFactory, RobustFactory, \
-     FormsFactory, LinksFactory, TitleFactory, \
-     RobustFormsFactory, RobustLinksFactory, RobustTitleFactory
+    Link, \
+    Factory, DefaultFactory, RobustFactory, \
+    FormsFactory, LinksFactory, TitleFactory, \
+    RobustFormsFactory, RobustLinksFactory, RobustTitleFactory
 
 # urllib2 work-alike interface.  This is a superset of the urllib2 interface.
 from ._urllib2 import *
@@ -143,17 +143,17 @@ from ._http import HeadParser
 from ._http import XHTMLCompatibleHeadParser
 from ._opener import ContentTooShortError, OpenerFactory, urlretrieve
 from ._response import \
-     response_seek_wrapper, seek_wrapped_response, make_response
+    response_seek_wrapper, seek_wrapped_response, make_response
 from ._rfc3986 import urljoin
 from ._util import http2time as str2time
 
 # cookies
 from ._clientcookie import Cookie, CookiePolicy, DefaultCookiePolicy, \
-     CookieJar, FileCookieJar, LoadError, request_host_lc as request_host, \
-     effective_request_host
+    CookieJar, FileCookieJar, LoadError, request_host_lc as request_host, \
+    effective_request_host
 from ._lwpcookiejar import LWPCookieJar, lwp_cookie_str
 # 2.4 raises SyntaxError due to generator / try/finally use
-if sys.version_info[:2] > (2,4):
+if sys.version_info[:2] > (2, 4):
     try:
         import sqlite3
     except ImportError:
@@ -199,7 +199,7 @@ from ._form import (
     SubmitControl,
     TextControl,
     TextareaControl,
-    )
+)
 
 # If you hate the idea of turning bugs into warnings, do:
 # import mechanize; mechanize.USE_BARE_EXCEPT = False
