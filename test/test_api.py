b89ac7c8e2241871a8593df5a4c993adda345960
sfall <sfall@my.tnstate.edu>
2014-04-04 21:15:17 -0500

    Workaround for "from mechanize import *" within function

--- test/test_api.py
+++ test/test_api.py
@@ -6,7 +6,9 @@ class ImportTests(unittest.TestCase):
     def test_import_all(self):
         # the following will raise an exception if __all__ contains undefined
         # classes
-        from mechanize import *
+        from mechanize import __all__ as things
+        for thing in things:
+            exec('from mechanize import '+thing)
 
 
 if __name__ == "__main__":
